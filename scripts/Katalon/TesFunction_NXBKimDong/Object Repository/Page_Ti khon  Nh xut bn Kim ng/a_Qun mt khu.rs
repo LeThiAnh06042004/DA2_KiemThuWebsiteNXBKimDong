35d737a7e052fe117d2b8af330806d256d3b160e
LeThiAnh06042004 <anhle06042004@gmail.com>
2025-01-14 21:54:45 +0700

    Done

---
+++ scripts/Katalon/TesFunction_NXBKimDong/Object Repository/Page_Ti khon  Nh xut bn Kim ng/a_Qun mt khu.rs
@@ -0,0 +1,154 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Qun mt khu</name>
+   <tag></tag>
+   <elementGuidId>7c4a1117-1038-4e30-82ef-39acc324eec9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#RecoverPassword</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@id='RecoverPassword']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;Quên mật khẩu?&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>eda919a9-8f34-40e7-8bb4-da6e3ae578cc</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>#recover</value>
+      <webElementGuid>01828889-f00d-4485-9ecf-90f408c7e907</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>RecoverPassword</value>
+      <webElementGuid>24d6ed3a-3a8e-4c67-b9cf-0b2056d000a5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Quên mật khẩu?</value>
+      <webElementGuid>42fbc797-c792-40d5-9cb1-4892145a0cc4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;RecoverPassword&quot;)</value>
+      <webElementGuid>19d217e4-97f3-4bf1-9511-d9760dd5ee37</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//a[@id='RecoverPassword']</value>
+      <webElementGuid>82f2badf-aa9a-4432-b9f8-458bfeabb40d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='customer_login']/p[4]/a</value>
+      <webElementGuid>22a1fc10-dafd-4be9-b9eb-24dcdd05672d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Quên mật khẩu?')]</value>
+      <webElementGuid>5fdfa0ed-3adb-4918-a085-fb38eb82a1f0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Đăng ký'])[2]/following::a[1]</value>
+      <webElementGuid>851c555f-b9a5-45a2-b7d0-40dfa1d7583f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Bỏ qua'])[1]/following::a[2]</value>
+      <webElementGuid>ac088f79-49f8-457c-a422-09013ac8120f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Cài đặt lại mật khẩu'])[1]/preceding::a[1]</value>
+      <webElementGuid>eebcc3ba-8abe-452b-853e-e57ffc414d6b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Email'])[2]/preceding::a[1]</value>
+      <webElementGuid>ee584243-5221-47c2-ae33-bccc4f734239</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Quên mật khẩu?']/parent::*</value>
+      <webElementGuid>4a3019cc-6b47-4452-b515-e2f56dc5a2b5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '#recover')]</value>
+      <webElementGuid>58873515-c93d-4c58-8ba2-ff0dae3ff941</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//p[4]/a</value>
+      <webElementGuid>8213b456-ddba-4283-802a-0cf53d5f6568</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '#recover' and @id = 'RecoverPassword' and (text() = 'Quên mật khẩu?' or . = 'Quên mật khẩu?')]</value>
+      <webElementGuid>83f546c6-9294-4077-8cfa-5a04f8b7a0e9</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ scripts/Katalon/TesFunction_NXBKimDong/Test Suites/TS_QuenMK.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_QuenMK</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>dbe5c69b-efab-47c8-8828-5cea044234b5</testSuiteGuid>
+   <testCaseLink>
+      <guid>61e18d03-b0e8-47f5-b824-f2c6b6af5707</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_QuenMK</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>05bac1c2-740a-40a9-8efb-adca361d799e</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Data_QuenMK</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>efad60f3-140e-4caa-89d7-ccb80b91d8b3</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
